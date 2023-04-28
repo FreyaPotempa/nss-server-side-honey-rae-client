14293301f16bc82f526d4ba6a508869184407c45
Freya Potempa <FreyaPotempa@gmail.com>
2023-04-28 16:36:31 -0500

    added default refs for user

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,37 +1,40 @@
-import React, { useRef } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { loginUser } from "../../managers/AuthManager"
-import "./Auth.css"
-
+import React, { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { loginUser } from "../../managers/AuthManager";
+import "./Auth.css";
 
 export const Login = () => {
-  const username = useRef()
-  const password = useRef()
-  const invalidDialog = useRef()
-  const navigate = useNavigate()
+  const username = useRef("bryan@nilson.com");
+  const password = useRef("nilson");
+  const invalidDialog = useRef();
+  const navigate = useNavigate();
 
   const handleLogin = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const user = {
       email: username.current.value,
-      password: password.current.value
-    }
-    loginUser(user)
-      .then(authInfo => {
-        if (authInfo.valid) {
-          localStorage.setItem("honeyrae", JSON.stringify(authInfo))
-          navigate("/")
-        } else {
-          invalidDialog.current.showModal()
-        }
-      })
-  }
+      password: password.current.value,
+    };
+    loginUser(user).then((authInfo) => {
+      if (authInfo.valid) {
+        localStorage.setItem("honeyrae", JSON.stringify(authInfo));
+        navigate("/");
+      } else {
+        invalidDialog.current.showModal();
+      }
+    });
+  };
 
   return (
     <main className="container--login">
       <dialog className="dialog dialog--auth" ref={invalidDialog}>
         <div>Username or password was not valid.</div>
-        <button className="button--close" onClick={e => invalidDialog.current.close()}>Close</button>
+        <button
+          className="button--close"
+          onClick={(e) => invalidDialog.current.close()}
+        >
+          Close
+        </button>
       </dialog>
       <section>
         <form className="form--login" onSubmit={handleLogin}>
@@ -39,16 +42,35 @@ export const Login = () => {
           <h2>Please sign in</h2>
           <fieldset>
             <label htmlFor="inputUsername"> Username </label>
-            <input ref={username} type="username" id="username" className="form-control" placeholder="Username address" required autoFocus />
+            <input
+              ref={username}
+              type="username"
+              id="username"
+              className="form-control"
+              placeholder="Username address"
+              required
+              autoFocus
+            />
           </fieldset>
           <fieldset>
             <label htmlFor="inputPassword"> Password </label>
-            <input ref={password} type="password" id="password" className="form-control" placeholder="Password" required />
+            <input
+              ref={password}
+              type="password"
+              id="password"
+              className="form-control"
+              placeholder="Password"
+              required
+            />
           </fieldset>
-          <fieldset style={{
-            textAlign: "center"
-          }}>
-            <button className="btn btn-1 btn-sep icon-send" type="submit">Sign In</button>
+          <fieldset
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <button className="btn btn-1 btn-sep icon-send" type="submit">
+              Sign In
+            </button>
           </fieldset>
         </form>
       </section>
@@ -56,5 +78,5 @@ export const Login = () => {
         <Link to="/register">Not a member yet?</Link>
       </section>
     </main>
-  )
-}
+  );
+};
