5bf255a4ce11ea18397c1850feec89d6d938f69b
Freya Potempa <FreyaPotempa@gmail.com>
2023-05-04 15:32:04 -0500

    filtering by status

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -1,56 +1,71 @@
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { isStaff } from "../../utils/isStaff"
-import { TicketCard } from "./TicketCard"
-import { getAllTickets, searchTicketsByStatus } from "../../managers/TicketManager"
-import "./Tickets.css"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { isStaff } from "../../utils/isStaff";
+import { TicketCard } from "./TicketCard";
+import {
+  getAllTickets,
+  searchTicketsByStatus,
+} from "../../managers/TicketManager";
+import "./Tickets.css";
 
 export const TicketList = () => {
-  const [active, setActive] = useState("")
-  const [tickets, setTickets] = useState([])
-  const navigate = useNavigate()
+  const [active, setActive] = useState("");
+  const [tickets, setTickets] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    getAllTickets().then((res) => setTickets(res))
-  }, [])
+    getAllTickets().then((res) => setTickets(res));
+  }, []);
 
   useEffect(() => {
-    const activeTicketCount = tickets.filter(t => t.date_completed === null).length
+    const activeTicketCount = tickets.filter(
+      (t) => t.date_completed === null
+    ).length;
     if (isStaff()) {
-      setActive(`There are ${activeTicketCount} open tickets`)
+      setActive(`There are ${activeTicketCount} open tickets`);
+    } else {
+      setActive(`You have ${activeTicketCount} open tickets`);
     }
-    else {
-      setActive(`You have ${activeTicketCount} open tickets`)
-    }
-  }, [tickets])
+  }, [tickets]);
 
   const toShowOrNotToShowTheButton = () => {
     if (isStaff()) {
-      return ""
+      return "";
+    } else {
+      return (
+        <button
+          className="actions__create"
+          onClick={() => navigate("/tickets/create")}
+        >
+          Create Ticket
+        </button>
+      );
     }
-    else {
-      return <button className="actions__create"
-        onClick={() => navigate("/tickets/create")}>Create Ticket</button>
-    }
-  }
+  };
 
   const filterTickets = (status) => {
-    searchTicketsByStatus(status).then((res) => setTickets(res))
-  }
+    searchTicketsByStatus(status).then((res) => setTickets(res));
+  };
 
-  return <>
-    <div>
-      <button onClick={() => filterTickets("done")}>Show Done</button>
-      <button onClick={() => filterTickets("all")}>Show All</button>
-    </div>
-    <div className="actions">{toShowOrNotToShowTheButton()}</div>
-    <div className="activeTickets">{active}</div>
-    <article className="tickets">
-      {
-        tickets.map(ticket => (
+  return (
+    <>
+      <div>
+        <button onClick={() => filterTickets("done")}>Show Done</button>
+        <button onClick={() => filterTickets("all")}>Show All</button>
+        <button onClick={() => filterTickets("unclaimed")}>
+          Show Unclaimed
+        </button>
+        <button onClick={() => filterTickets("inprogress")}>
+          Show In Progress
+        </button>
+      </div>
+      <div className="actions">{toShowOrNotToShowTheButton()}</div>
+      <div className="activeTickets">{active}</div>
+      <article className="tickets">
+        {tickets.map((ticket) => (
           <TicketCard key={`ticket--${ticket.id}`} ticket={ticket} />
-        ))
-      }
-    </article>
-  </>
-}
+        ))}
+      </article>
+    </>
+  );
+};
